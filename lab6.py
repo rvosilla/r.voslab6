e3f3ae02b22c557f1d5947adf4f87f66c0c75588
rvosilla <127446886+rvosilla@users.noreply.github.com>
2023-03-20 14:36:33 -0400

    Update lab6.py
    added option 2 and comments

--- lab6.py
+++ lab6.py
@@ -15,12 +15,12 @@ def encode(password):
 
 def decoder(password):
     end_result = ''
-    for i in range(len(password)):
-        a = int(password[i])
+    for i in range(len(password)):  # iterates through index
+        a = int(password[i])  # get integer value at index
 
-        b = a - 3
+        b = a - 3  # decrement by 3
         b = str(b)
-        end_result += b
+        end_result += b  # add back to original password
     return end_result
 
 if __name__ == "__main__":
@@ -30,11 +30,12 @@ if __name__ == "__main__":
         print("Please enter an option:", end = " ")
         option = int(input())
         if option == 1:
+            # encodes password input
             password = input("Please enter your password to encode:")
             encode(password)
             print("Your password has been encoded and stored!")
         elif option == 2:
-            # print(f"The encoded password is {encoded_pw}, and the original password is {password}.")
-            pass
+            # decodes and prints the existing encoded password
+            print(f"The encoded password is {encoded_password}, and the original password is {decoder(encoded_password)}.")
         elif option == 3:
             menu = False
